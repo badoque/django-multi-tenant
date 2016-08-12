558807092838eefe692e5fc66e258c3b6f207752
Túlio Paulo Lages da Siva <tulio.plds@gmail.com>
2016-08-12 12:50:48 -0300

    Multi tenant model (WIP)

--- multi_tenant/models.py
+++ multi_tenant/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from .managers import TenantModelManager
-
+from django.conf import settings
 
 class Theme(models.Model):
     """
@@ -24,7 +24,7 @@ class Tenant(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     active_until = models.DateField(null=True, blank=True)
     theme = models.ForeignKey(Theme, null=True, blank=True)
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return self.name
@@ -47,7 +47,19 @@ class TenantModel(models.Model):
     project tables.
     """
 
-    tenant = models.ForeignKey(Tenant, related_name='tenant')
+    tenant = models.ForeignKey(Tenant)
+    objects = TenantModelManager()
+
+    class Meta:
+        abstract = True
+
+class MultiTenantModel(models.Model):
+    """
+    This is a abstract model to provide a shared database aproach to the
+    project tables.
+    """
+
+    tenants = models.ManyToManyField(Tenant, related_name='tenants')
     objects = TenantModelManager()
 
     class Meta:
